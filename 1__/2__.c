163bbf9f1d0252bce7032e91ef048ceb2a2dd21f
leejinwook69 <leejinwook69@gmail.com>
2021-03-16 02:23:24 +0900

    새로하나

---
+++ 1__/2__.c
@@ -0,0 +1,25 @@
+//이름을 입력 받고 / 맞는지 확인을 한 후 / 식별번호를 입력 받고 / 확인을 한다
+
+
+#include <stdio.h>
+
+void regi($$){
+    printf("your name ? : ");
+    scanf("%s", reg);
+}
+
+
+int main(void){
+    
+    int reg[50];
+    int name;
+    int pw;
+
+    regi();
+
+    printf("name : %s\n 1.yes 2.no");
+
+
+    return 0;
+
+}
