8cb12c36e687c4bec27cd3de80ab6bc23378a1a0
leejinwook69 <leejinwook69@gmail.com>
2021-03-18 00:34:40 +0900

    나중에 고쳐

--- 1__/2__.c
+++ 1__/2__.c
@@ -3,25 +3,68 @@
 
 #include <stdio.h>
 
-void regi(int * ptr){
+void regi(int * ptr)
+{
     printf("your name ? : ");
-    scanf("%s", ptr);
+    scanf("%s", *(ptr));
+  
 }
 
+void pword(int * ptr)
+{
+    printf("password ? : ");
+    scanf("%s", *(ptr));
+}
 
-int main(void){
+int main(void)
+{
     
-    int *reg;
+    int * reg;
     int r;
+    int n=0;
     int name;
-    int pw;
+    int * pw;
+    int p;
 
+    int check;
+
+//reg는 r의 주소값, pw는 p의 주소값
+    
     reg = &r;
+    pw = &p;
 
-    regi(reg);
+//이름 입력
+    while(n != name)
+    {
+        regi(reg);
+        printf("name : %s\n 1.yes 2.no\n", r);
+        scanf("%d", &n);
+    }
 
-    printf("name : %s\n 1.yes 2.no\n", r);
+//pw입력
+    while(n != p)
+    {
+        pword(pw);
+        printf("password check : ");
+        scanf("%s", &n);
+    }
 
+//확인
+    for(check = 1; check != name;)
+    {
+        printf("success .\n\nname : ");
+        scanf("%s", &check);
+        if(check != name)
+            printf("no name matched\n");
+    }
+
+    for(check = 1; check != p;)
+    {
+        printf("password : ");
+        scanf("%s", &check);
+        if(check != p)
+            printf("wrong password\n");
+    }
 
     return 0;
 
