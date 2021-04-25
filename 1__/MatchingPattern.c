bf03074486c6b388ff43230a1811696115683779
leejinwook69 <leejinwook69@gmail.com>
2021-04-26 08:27:11 +0900

    Missingword

---
+++ 1__/MatchingPattern.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+//getline = 문자열의 길이 출력
+//strindex = 문자열 s 안에 있는 t문자열의 인덱스 번호 출력
+
+
+int getline1(char s[], int lim) {
+    int c, i;
+    i = 0;
+    
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    
+    if (c == '\n') 
+        s[i++] = c;
+
+    s[i] = '\0';
+
+    return i;
+}
+
+int strindex(char s[], char t[]) {
+    int i, j, k;
+
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++) 
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+}
+
+
+int main(void) {
+    char line[MAXLINE];
+    int found = 0;
+    char pattern[] = "ould";
+
+    while (getline1(line, MAXLINE) > 0) {
+        if (strindex(line, pattern) >= 0) {
+            printf("%s", line);
+            found++;
+        }
+
+    }
+    return found;
+}
