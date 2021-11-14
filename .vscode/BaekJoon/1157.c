dda4b2d5d0359474423f8007d57b22c26af247d3
leejinwook69 <80254252+leejinwook69@users.noreply.github.com>
2021-11-15 01:51:58 +0900

    good

--- .vscode/BaekJoon/1157.c
+++ .vscode/BaekJoon/1157.c
@@ -4,6 +4,7 @@ char arr[1000000];
 int arr2[26];
 int max;
 int maxalpha;
+int scount;
 
 int main(void)
 {
@@ -14,23 +15,29 @@ int main(void)
         if(arr[i]-97 < 0)
         {
             arr2[arr[i]-65]++;
-        }
-        if(arr[i]-97 >= 0)
+        }else
         {
             arr2[arr[i]-97]++;
         }
     }
 
+
     for(int i = 0; i<26; i++)
     {
-        if(max != 0 && max == arr2[i])
-        {
-            maxalpha = 63;
-            break;
-        }else if(arr2[i] > max)
+        if(arr2[i] > max)
         {
             max = arr2[i];
             maxalpha = i+65;
+        }
+    }
+    for(int i = 0; i<26; i++)
+    {
+        if(arr2[i]==max)
+            scount++;
+        if(scount >=2)
+        {
+            maxalpha = 63;
+            break;
         }
     }
         printf("%c", maxalpha);
