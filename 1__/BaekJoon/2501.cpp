19eb5c6e7a0e27acb9a068f0bc20c508750b2c4e
Lee_Desktop <leejinwook69@gmail.com>
2024-10-12 21:38:10 +0900

    okay

--- 1__/BaekJoon/2501.cpp
+++ 1__/BaekJoon/2501.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void)
 {
-    
+    int n = 0, k = 0;
+    vector<int> v;
+    v.reserve(1000);
+
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            v.push_back(i);
+        }
+    }
+
+    if (v.size() < k)
+    {
+        cout << 0 << '\n';
+    }
+    else
+    {
+        cout << v.at(k - 1);
+    }
 }
